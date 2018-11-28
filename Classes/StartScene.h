10690069f059b9b06b153eb107d0f2cef96e0a83
ZengNuo <16nzeng2@stu.edu.cn>
2018-11-28 23:32:04 +0800

    Rebuild StartScene

--- Classes/StartScene.h
+++ Classes/StartScene.h
@@ -13,12 +13,16 @@ public:
 
 	void createMenu();
 
+
 	void menuStartCallback(cocos2d::Ref* pSender);
 	void menuSettingCallback(cocos2d::Ref* pSender);
 	void menuAboutCallback(cocos2d::Ref* pSender);
 	void menuExitCallback(cocos2d::Ref* pSender);
 
 	CREATE_FUNC(StartScene);
+
+private:
+	cocos2d::MenuItemSprite* createSprite(string text, const cocos2d::ccMenuCallback& callback);
 };
 
 #endif // __START_SCENE_H__
