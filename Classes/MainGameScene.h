6eac0c10df9124001c8af84ad369b5c264171868
LUEVE <724818484@qq.com>
2018-12-01 01:41:48 +0800

    重构了一部分代码，建立SG_Action
    SG_Action 类用来保存统一所有的 自建Action

--- Classes/MainGameScene.h
+++ Classes/MainGameScene.h
@@ -7,11 +7,12 @@
 #include "Block/SG_Game.h"
 #include <ctime>
 #include <fstream>
+#include "SimpleAudioEngine.h"
 
 
 // 游戏开始MAX_RUNNING_TIME秒之后结束
 #define MAX_RUNNING_TIME 15
-
+using std::string;
 using namespace std;
 class MainGameScene : public cocos2d::Scene
 {
@@ -19,24 +20,34 @@ public:
 	static std::chrono::steady_clock::time_point pauseStart;
 	static std::chrono::steady_clock::time_point  pauseEnd;
 	static chrono::steady_clock::duration pauseDuration;
+
 	SG_Note* comboPlace;
 	SG_Game game;
 	int comboNumber = 0;
 	ifstream scoreIfs;
 	std::chrono::steady_clock::time_point startPoint;
+	cocos2d::Label*comboNumberLabel, *gameScoreLabel, *addedGameScoreLabel;
+	Size visibleSize;
+
 	MainGameScene();
+	CREATE_FUNC(MainGameScene);
+
+
+	string bgmSongName = "Wiwi Kuan- Made in PixiTracker.mp3";
+	vector<string> musicEffect = {"MusicEffect\\Effect_cai.mp3","MusicEffect\\Effect_ruo.mp3","MusicEffect\\Effect_xing.mp3","MusicEffect\\Effect_qiang.mp3"};
+	void preloadSoundResource(CocosDenshion::SimpleAudioEngine* sae);
+	virtual bool init();
 	static cocos2d::Scene* createScene();
 	bool CreatAllSpirtInSong();
-	virtual bool init();
+	double getDuration(const std::chrono::steady_clock::time_point &start, const std::chrono::steady_clock::time_point &end);
 	void menuBackCallBack(Ref * pSender);
 	void setJudgeAnimation(Animation* animation, int i);
 	void oneFrameForward();
 	void appearNotes();
 	void update(float dt);
-	cocos2d::Label*comboNumberLabel, *gameScoreLabel, *addedGameScoreLabel;
-	CREATE_FUNC(MainGameScene);
-	double getDuration(const std::chrono::steady_clock::time_point &start, const std::chrono::steady_clock::time_point &end);
-	//time_t start_time, now_time;
+	void reFreshComboLebal();
+	void reFreshGameScoreLabel(long long addedScore);
+
 private:
 	int frameCnt = 0;
 };
